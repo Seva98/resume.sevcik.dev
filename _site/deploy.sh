b056e5253fc44122b01853de016e2e8f3fc8aed5
Ondrej Sevcik <Ondrej Sevcik>
2020-03-20 10:54:54 +0100

    2020-03-20 update

--- _site/deploy.sh
+++ _site/deploy.sh
@@ -1,11 +1,16 @@
-git add -A
-git commit -m "`date +'%Y-%m-%d'` blog post"
-git push
+# git add -A
+# git commit -m "`date +'%Y-%m-%d'` blog post"
+# git push
+# jekyll build
+# cd _site
+# touch .nojekyll
+# git init
+# git add -A
+# git commit -m "`date +'%Y-%m-%d'` blog post"
+# git remote add origin git@github.com:Seva98/sevcik.dev.git
+# git push -f -u origin master
+
 jekyll build
-cd _site
-touch .nojekyll
-git init
 git add -A
-git commit -m "`date +'%Y-%m-%d'` blog post"
-git remote add origin git@github.com:Seva98/sevcik.dev.git
-git push -f -u origin master
+git commit -m "`date +'%Y-%m-%d'` update"
+git subtree push --prefix _site origin gh-pages
