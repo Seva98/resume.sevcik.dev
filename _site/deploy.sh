35cfc018ac7fd2ec82d8fb0eba7baac9bf5ee009
Ondrej Sevcik <Ondrej Sevcik>
2020-03-20 10:52:25 +0100

    2020-03-20 blog post

--- _site/deploy.sh
+++ _site/deploy.sh
@@ -1,10 +1,11 @@
-git commit -a -m "`date +'%Y-%m-%d'` blog post"
+git add -A
+git commit -m "`date +'%Y-%m-%d'` blog post"
 git push
 jekyll build
 cd _site
 touch .nojekyll
 git init
 git add -A
-git commit -a -m "`date +'%Y-%m-%d'` blog post"
+git commit -m "`date +'%Y-%m-%d'` blog post"
 git remote add origin git@github.com:Seva98/sevcik.dev.git
 git push -f -u origin master
