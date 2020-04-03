e25a40b35b480df3a91d11a3dbaf8e3307456c33
Ondrej Sevcik <Ondrej Sevcik>
2020-04-03 19:31:06 +0200

    2020-04-03 update

--- deploy.sh
+++ deploy.sh
@@ -7,22 +7,4 @@ sed -i '' '/^_site/d' .gitignore
 git add _site
 git commit -m "`date +'%Y-%m-%d'` update"
 git push origin `git subtree split --prefix _site master`:gh-pages --force
-#git subtree push --prefix _site origin gh-pages
 echo "_site/" >> .gitignore
-
-# # Setup gh-pages
-# rm -rf _site
-# sed -i '' '/^_site/d' .gitignore
-# git worktree add _site gh-pages
-# jekyll build
-# # Push gh-pages
-# cd _site
-# git add .
-# git commit -a -m "`date +'%Y-%m-%d'` update"
-# git push origin gh-pages --force
-# cd ..
-# echo "_site/" >> .gitignore
-
-
-
-
