b93f569216c0aab98a347584a43684c8573f1915
Ondrej Sevcik <Ondrej Sevcik>
2020-04-03 16:43:12 +0200

    2020-04-03 update

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,8 @@ git commit -m "`date +'%Y-%m-%d'` update"
 git push origin master
 cd _site
 git init
+git remote add origin git@github.com:Seva98/sevcik.dev.git
 git add -A
 git commit -m "`date +'%Y-%m-%d'` update"
-git push --force git@github.com:Seva98/sevcik.dev.git gh-pages
+git push -f origin gh-pages
+rm -rf .git
