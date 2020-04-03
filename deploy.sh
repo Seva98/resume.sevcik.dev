92e52e94f207520db4d52526698590809da78347
Ondrej Sevcik <Ondrej Sevcik>
2020-04-03 16:48:58 +0200

    2020-04-03 update

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,5 @@
+jekyll build
 git add -A
 git commit -m "`date +'%Y-%m-%d'` update"
-git push origin master
-rm -rf _site/*
-jekyll build
 git subtree push --prefix _site origin gh-pages
+git push origin master
