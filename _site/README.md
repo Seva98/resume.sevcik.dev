2787d3dce0990db6869c8bb0c07117278f74fdd9
Ondrej Sevcik <Ondrej Sevcik>
2020-04-04 13:00:03 +0200

    2020-04-04 update

--- _site/README.md
+++
@@ -1,5 +0,0 @@
-# Sevcik.dev
-
-`master` branch includes source code of Jekyll website
-
-`gh-pages` branch includes built website that is hosted on GitHub Pages
--- deploy.sh
+++ deploy.sh
@@ -3,8 +3,8 @@ git add -A
 git commit -m "`date +'%Y-%m-%d'` update"
 git push origin master
 
-sed -i '' '/^_site/d' .gitignore
+#sed -i '' '/^_site/d' .gitignore
 git add _site
 git commit -m "`date +'%Y-%m-%d'` update"
 git push origin `git subtree split --prefix _site master`:gh-pages --force
-echo "_site" >> .gitignore
+#echo "_site" >> .gitignore
