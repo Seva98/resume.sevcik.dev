6fb54d6aaf7d732d562e1625b8ce28f0ec619290
Ondrej Sevcik <Ondrej Sevcik>
2020-04-04 13:08:31 +0200

    2020-04-04 update

--- _site/deploy.sh
+++ _site/deploy.sh
@@ -7,5 +7,5 @@ git push origin master
 sed -i '' '/^_site/d' .gitignore
 git add _site
 git commit -m "`date +'%Y-%m-%d'` update"
-git push origin `git subtree split --prefix _site master`:gh-pages --force
+git push origin `git subtree split --prefix _site master`:refs/heads/gh-pages --force
 echo "_site" >> .gitignore
