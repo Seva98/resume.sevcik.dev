c437026d48d6afeb4a428ef8f37b65b9411887d3
Ondrej Sevcik <Ondrej Sevcik>
2020-07-12 12:43:47 +0200

    2020-07-12 update

--- svelte-components/src/HelloWorld.svelte
+++ svelte-components/src/HelloWorld.svelte
@@ -1,1 +1,9 @@
-<p>Hello World!</p>
+<script>
+  import Nested from "./Nested.svelte";
+  let name = "<b>We Are</b>";
+  let src = "http://127.0.0.1:4000/assets/images/resized/1400/selfie.jpg";
+</script>
+
+<div>
+  <div id="svelte-works" />
+</div>
---
+++ svelte-components/src/Nested.svelte
@@ -0,0 +1,1 @@
+<p>Nested test</p>
